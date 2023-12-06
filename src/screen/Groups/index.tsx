e450daf8f7eef24f04e4f3a1e23f1b6e635b1e03
Joao Vinicius <joaovvs@gmail.com>
2023-12-06 08:51:55 -0300

    first commit

---
+++ src/screen/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export function Groups() {
+  return (
+    <View >
+      <Text>Groups</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
