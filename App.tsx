5939150a9892a4d7e17f39345fd4ace7d05d8e89
Joao Vinicius <joaovvs@gmail.com>
2023-12-06 13:33:22 -0300

    theme and statusbar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+
+import { ThemeProvider } from 'styled-components';
+
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 import { Groups } from '@screens/Groups';
+import { Loading } from '@components/Loading';
+
+import theme from './src/theme'
 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+
+
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+            <StatusBar 
+                style="light" 
+                backgroundColor='transparent'
+                translucent
+            />
+        { fontsLoaded ? <Groups />: <Loading/>}
+    </ThemeProvider>
   );
 }
 
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,14 +2,13 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex:1;
-    background-color: #000;
-    align-items: center;
-    justify-content: center;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+    padding: 24px;
     
 
 `;
 export const Title = styled.Text`
-    color: #fff;
+    color: ${({theme})=> theme.COLORS.GRAY_100};
     font-size: 32px;
     font-weight: bold;
 
